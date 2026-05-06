3631be8c11eca54814704486abde2d7705af16ea
Ethan Saldanha <ethans201@nycstudents.net>
2026-05-06 10:31:31 -0400

    eviss

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import random
 from flask import Flask
-from flask_socketio import SocketIO, join_room, leave_room, emit, request as socketio_request
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask import request as socketio_request
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "gabagoobakey"
